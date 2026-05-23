7c101fd84824d94500e677342fd7425908689d0b
anushkachandra933 <anushkachandra933@gmail.com>
2026-05-23 08:30:50 +0530

    added new file

--- Patterns/HollowTriangle.java
+++ Patterns/HollowTriangle.java
@@ -2,10 +2,12 @@ package Patterns;
 public class HollowTriangle {
     public static void main(String[] args) {
         int n = 4;
+        //spaces
         for(int i=1; i<=n; i++){
             for(int j=1; j<=n-i; j++){
                 System.out.print(" ");
             }
+            //stars
             for(int j=1; j<=i; j++){
                 if(j == 1|| j==i || i==n){
 
