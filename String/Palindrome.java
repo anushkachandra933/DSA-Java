08ae16c0aab61330036b3255f953cacfb91938b3
anushkachandra933 <anushkachandra933@gmail.com>
2026-06-01 20:58:24 +0530

    added new file

--- String/Palindrome.java
+++ String/Palindrome.java
@@ -4,7 +4,7 @@ public class Palindrome {
     public static boolean palindrome(String str){
         int n = str.length();
         int mid = n/2;
-        for(int i=0; i<=mid; i++){
+        for(int i=0; i<mid; i++){
             if(str.charAt(i) != str.charAt(n-i-1)){
                 return false;
             }
@@ -13,6 +13,6 @@ public class Palindrome {
     }
 
     public static void main(String[] args) {
-        System.out.println(palindrome("madeam"));
+        System.out.println(palindrome("madam"));
     }
 }
